033f1b093ee574a1119bc6708e5d1564fa6998a1
Q-Max <a1u1usul3@yahoo.com.tw>
2015-05-14 23:05:35 +0800

    modify not much

--- hw2/client_echo_udp.c
+++ hw2/client_echo_udp.c
@@ -9,12 +9,17 @@
 
 
 #define MAXLINE 4096
+#define connect 0
+
 void dg_cli(FILE *fp, int sockfd, struct sockaddr *pservaddr, socklen_t servlen);
 
 int main(int argc, char **argv)
 {
 	int sockfd;
 	struct sockaddr_in servaddr;
+	struct timeval tv;
+	tv.tv_sec = 3;
+	tv.tv_usec = 0;
 	if (argc != 3){
 		printf("usage: tcpcli <IP address> <Port>\n");
 		exit(0);
@@ -25,18 +30,32 @@ int main(int argc, char **argv)
 	servaddr.sin_port = htons(SERV_PORT);
 	inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
 	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	if(setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv)) < 0) {
+		perror("Error");
+	}
 	dg_cli(stdin, sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
 	exit(0);
 }
 void dg_cli(FILE *fp, int sockfd, struct sockaddr *pservaddr, socklen_t servlen) {
 	int n;
 	char sendline[MAXLINE], recvline[MAXLINE + 1];
+	sendline[0] = connect;
+	sendto(sockfd, sendline, 1, 0, pservaddr, servlen);
+	if((n = recvfrom(sockfd, recvline, MAXLINE, 0, NULL, NULL)) < 0){
+		printf("timeout\n");		
+	}
+	recvline[n] = '\0';
+	/* null terminate */
+	//printf("%s %d\n",recvline,strlen(recvline));
+	fputs(recvline, stdout);
 	while (fgets(sendline, MAXLINE, fp) != NULL) {
 		sendto(sockfd, sendline, strlen(sendline), 0, pservaddr, servlen);
-		n = recvfrom(sockfd, recvline, MAXLINE, 0, NULL, NULL);
-		recvline[n] = 0;
+		if((n = recvfrom(sockfd, recvline, MAXLINE, 0, NULL, NULL)) < 0){
+			printf("timeout\n");
+			continue;
+		}
+		recvline[n] = '\0';
 		/* null terminate */
-		printf("UDP receive : ");
 		fputs(recvline, stdout);
 	}
 }
--- hw2/server_echo_udp.c
+++ hw2/server_echo_udp.c
@@ -1,21 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <strings.h> /* bzero()*/
+#include <string.h> /* strcpy*/
 #include <sys/socket.h> /* socket*/
 #include <sys/select.h>
 #include <time.h>
 #include <arpa/inet.h> /* ipv4*/
 #include <netinet/in.h> /* htonl ntohl htons ntohs*/
 #include <errno.h>
+#include <vector>
+#include <algorithm>
+#include <string>
 
 #define LISTENQ 256
 #define MAXLINE 4096
+#define WELCOME "*************Welcome*****************\n[R]egister\t[L]ogin\n"
+
+#define connect 0
+#define regist 1
+#define login 2
+#define regist2 3
+#define ack 4
+struct account{
+	unsigned int id;
+	std::string name;
+	std::string pw;
+};
+
+std::vector<account> accounts;
 
 int max(int num1, int num2) ;
 int main(int argc, char **argv)
 {
 	int i, maxi, maxfd, listenfd, connfd, sockfd,udpfd;
 	const int on=1;
+	unsigned int id = 1;
+	int state = 0;
 	int nready, client[FD_SETSIZE];
 	ssize_t n;
 	socklen_t len;
@@ -44,7 +64,13 @@ int main(int argc, char **argv)
 	/* index into client[] array */
 	/* for create UDP socket*/
 	struct sockaddr_in servaddr;
+	struct timeval tv;
+	tv.tv_sec = 3;
+	tv.tv_usec = 0;
 	udpfd = socket(AF_INET, SOCK_DGRAM, 0);
+	// if(setsockopt(udpfd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv)) < 0) {
+	// 	perror("Error");
+	// }
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
@@ -63,7 +89,27 @@ int main(int argc, char **argv)
 		
 		if (FD_ISSET(udpfd, &rset)) {
 			len = sizeof(servaddr);
-			n = recvfrom(udpfd, mesg, MAXLINE, 0, (struct sockaddr *) &servaddr, &len);
+			if((n = recvfrom(udpfd, mesg, MAXLINE, 0, (struct sockaddr *) &servaddr, &len)) < 0 ){
+				printf("timeout\n");
+			}
+			// mesg[0] = ACK:
+			// sendto(udpfd, mesg, 1, 0, (struct sockaddr *) &servaddr, len);
+			printf("connection from %s, port %hu\n",inet_ntop(AF_INET, &( servaddr.sin_addr), dst, INET_ADDRSTRLEN),ntohs(servaddr.sin_port));
+			if(mesg[0]==connect){
+				strcpy(mesg, WELCOME);
+				sendto(udpfd, mesg, strlen(mesg), 0, (struct sockaddr *) &servaddr, len);
+				continue;
+			}
+			else if(mesg[0]==regist){
+				strcpy(mesg , "Please input your username and password by <username>/<password> : \n");
+				sendto(udpfd, mesg, strlen(mesg), 0, (struct sockaddr *) &servaddr, len);
+				continue;
+			}
+			else if(mesg[0]==login){
+				strcpy(mesg, WELCOME);
+				sendto(udpfd, mesg, strlen(mesg), 0, (struct sockaddr *) &servaddr, len);
+				continue;
+			}
 			sendto(udpfd, mesg, n, 0, (struct sockaddr *) &servaddr, len);
 		}
 	}
