67b7fa4f58676457e8d66ff8692a12b3f92eaeb3
Q-Max <a1u1usul3@yahoo.com.tw>
2015-05-14 08:18:25 +0800

    udp done

--- hw2/server_echo_udp.c
+++ hw2/server_echo_udp.c
@@ -28,21 +28,22 @@ int main(int argc, char **argv)
 	}
 	int SERV_PORT=atoi(argv[1]);
 	char line[MAXLINE];
-	socklen_t clilen;
-	struct sockaddr_in cliaddr, servaddr;
-	listenfd = socket(AF_INET, SOCK_STREAM, 0);
-	bzero(&servaddr, sizeof(servaddr));
-	servaddr.sin_family= AF_INET;
-	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	servaddr.sin_port= htons(SERV_PORT);
-	setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
-	bind(listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
-	listen(listenfd, LISTENQ);
+	//socklen_t clilen;
+	// struct sockaddr_in cliaddr, servaddr;
+	// listenfd = socket(AF_INET, SOCK_STREAM, 0);
+	// bzero(&servaddr, sizeof(servaddr));
+	// servaddr.sin_family= AF_INET;
+	// servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	// servaddr.sin_port= htons(SERV_PORT);
+	//setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+	// bind(listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
+	//listen(listenfd, LISTENQ);
 	
 	/* initialize */
 	maxi = -1;
 	/* index into client[] array */
 	/* for create UDP socket*/
+	struct sockaddr_in servaddr;
 	udpfd = socket(AF_INET, SOCK_DGRAM, 0);
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
@@ -50,66 +51,20 @@ int main(int argc, char **argv)
 	servaddr.sin_port = htons(SERV_PORT);
 	bind(udpfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
 
-	maxfd = max(listenfd,udpfd)+1;
+	maxfd = udpfd + 1;
 	for (i = 0; i < FD_SETSIZE; i++)client[i] = -1;
 	/* -1 indicates available entry */
 	FD_ZERO(&allset);
 	FD_SET(listenfd, &allset);
 	for ( ; ; ) {
 		rset = allset;
-		FD_SET(listenfd, &rset);
 		FD_SET(udpfd, &rset);
-		/* structure assignment */
-		if((nready = select(maxfd+1, &rset, NULL, NULL, NULL))<0){
-			if(errno == EINTR)
-				continue;
-			else
-				puts("select error");
-		} /*# of ready descriptors */
-		if (FD_ISSET(listenfd, &rset)) {/* new client connection */
-
-			clilen = sizeof(cliaddr);
-			connfd = accept(listenfd, (struct sockaddr *) &cliaddr, &clilen);
-			printf("connection from %s, port %hu\n",inet_ntop(AF_INET, &( cliaddr.sin_addr), dst, INET_ADDRSTRLEN),ntohs(cliaddr.sin_port));
-			for (i = 0; i < FD_SETSIZE; i++)
-				if (client[i] < 0) {
-					client[i] = connfd;
-					/* save descriptor */
-					break;
-				}
-			if (i == FD_SETSIZE)printf("too many clients"); /*cannot accept clients*/
-			FD_SET(connfd, &allset);
-			if (connfd > maxfd)
-				maxfd = connfd;
-			if (i > maxi)
-				maxi = i;
-			if (--nready <= 0)
-				continue;
-		}
-		for (i = 0; i <= maxi; i++) {
-			if ( (sockfd = client[i]) < 0)
-				continue;	/* check all clients for data */
-					/*Skip empty client[]*/
-			if (FD_ISSET(sockfd, &rset)) {
-				if ( (n = read(sockfd, line, MAXLINE)) == 0) {
-				/* connection closed by client */
-				close(sockfd);
-				FD_CLR(sockfd, &allset);
-				client[i] = -1;
-				} else{
-					printf("connection from %s, port %hu\n",inet_ntop(AF_INET, &( cliaddr.sin_addr), dst, INET_ADDRSTRLEN),ntohs(cliaddr.sin_port));
-					puts(line);
-					write(sockfd, line, n);
-				}
-				if (--nready <= 0)
-				break;
-				/* no more readable descriptors */
-			}
-		}
+		 /*# of ready descriptors */
+		
 		if (FD_ISSET(udpfd, &rset)) {
-			len = sizeof(cliaddr);
-			n = recvfrom(udpfd, mesg, MAXLINE, 0, (struct sockaddr *) &cliaddr, &len);
-			sendto(udpfd, mesg, n, 0, (struct sockaddr *) &cliaddr, len);
+			len = sizeof(servaddr);
+			n = recvfrom(udpfd, mesg, MAXLINE, 0, (struct sockaddr *) &servaddr, &len);
+			sendto(udpfd, mesg, n, 0, (struct sockaddr *) &servaddr, len);
 		}
 	}
 }
